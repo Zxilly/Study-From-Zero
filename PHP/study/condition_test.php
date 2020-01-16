0f43920d09805c88100dcfcb6cb016059cd01c96
Zxilly <zhouxinyu1001@gmail.com>
2020-01-16 21:23:45 +0800

    delete

---
+++ PHP/study/condition_test.php
@@ -0,0 +1,7 @@
+<?php
+$time = 5;
+if ($time < 24) {
+    echo $time;
+} else {
+    echo -$time;
+}
--- PHP/study/string_test.php
+++ PHP/study/string_test.php
@@ -1,1 +1,8 @@
 <?php
+$txt = "This is a long string.";
+$txt1 = "This is also a long string.";
+echo strlen($txt1);
+echo "<br>";
+echo $txt . " " . $txt1;
+echo "<br>";
+echo $txt;
