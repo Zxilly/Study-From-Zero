cf215c640d173c53095a8b4efa7cbf152a023ff5
Zxilly <zhouxinyu1001@gmail.com>
2020-02-18 15:13:30 +0800

    2.18

--- C++/grammar/var_test.cpp
+++ C++/grammar/var_test.cpp
@@ -5,15 +5,17 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int a;
     long b;
     long long c;
     float d;
     double e;
     unsigned long long f;
-    int sum=0L;
+    int sum = 0L;
+    _Bool booltype = 1;
+    _Bool booltest = 5;
+    cout << booltest << " " << booltype << endl;
 
-    cout<<sum<<endl;
+    cout << sum << endl;
 }
--- Python/grammar/basic/output_test.py
+++ Python/grammar/basic/output_test.py
@@ -1,2 +1,10 @@
+f = {1: 'Google', 2: 'Microsoft', 3: 'Amazon', 'expensive': 'Apple'}
+
+'''
 for x in range(1,11):
     print('{0:5d}{1:5d}{2:5d}'.format(x,x*x,x*x*x))
+'''
+
+print('1:{0[1]}'.format(f))
+
+print('{expensive}'.format(**f))
