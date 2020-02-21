2e92e6cdfb80df70e7e6126b8ca14d17d163ebe7
Zxilly <zhouxinyu1001@gmail.com>
2020-02-21 23:16:36 +0800

    2.21

---
+++ C++/grammar/file_test.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 12009 on 2020/2/21.
+//
+#include <bits/stdc++.h>
+
+int main()
+{
+    int a;
+    char name[100];
+    FILE * fp;
+    scanf("%s",name);
+    fp = fopen(name,"r");
+
+    if(fp == NULL)
+    {
+        printf("Cannot open\n");
+        exit(1);
+    }
+    while((a=getc(fp))!=EOF)
+    {
+        putchar(a);
+        putchar('\n');
+        printf("run once\n");
+    }
+
+    fclose(fp);
+    return 0;
+}
---
+++ C++/grammar/function_test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 12009 on 2020/2/21.
+//
+#include <bits/stdc++.h>
+
+void test(int a,int b)
+{
+    printf("%d",a+b);
+}
+
+int main()
+{
+    test(3,4);
+}
