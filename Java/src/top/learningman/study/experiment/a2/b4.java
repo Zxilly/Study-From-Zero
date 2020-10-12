d4cc405a623a22aa948939aa97bcfb7d68101c8d
zxilly <zhouxinyu1001@gmail.com>
2020-10-12 20:05:26 +0800

    线性筛Java实现

--- Java/src/top/learningman/study/experiment/a2/b4.java
+++ Java/src/top/learningman/study/experiment/a2/b4.java
@@ -1,12 +1,34 @@
 package top.learningman.study.experiment.a2;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class b4 {
+    static boolean[] check = new boolean[1001];
+    static ArrayList<Integer> prime = new ArrayList<>();
+
+
     public static void main(String[] args){
-        HashSet<Integer> hs = new HashSet<Integer>();
-        for(int i=2;i<=1000;i++){
-            hs.add(i);
+        for(int i=0;i<=1000;i++){
+            check[i]=true;
         }
+
+        for(int i=2;i<=1000;i++){
+            if(check[i]){
+                prime.add(i);
+            }
+
+            for(int j=0;j<prime.size()&&i*prime.get(j)<=1000;j++){
+                check[i*prime.get(j)]=false;
+                if(i%prime.get(j)==0){
+                    break;
+                }
+            }
+        }
+
+        for(int out:prime){
+            System.out.print(out+" ");
+        }
+        System.out.print("\n");
+        System.out.println(prime.size());
     }
 }
---
+++ Java/src/top/learningman/study/experiment/a2/b5.java
@@ -0,0 +1,4 @@
+package top.learningman.study.experiment.a2;
+
+public class b5 {
+}
