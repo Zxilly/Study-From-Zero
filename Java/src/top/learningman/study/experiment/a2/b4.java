e94f3ff29a1bf32b18802085ba606f3c591b24b8
zxilly <zhouxinyu1001@gmail.com>
2020-10-12 09:50:38 +0800

    java experiment 2

---
+++ Java/src/top/learningman/study/experiment/a2/b4.java
@@ -0,0 +1,12 @@
+package top.learningman.study.experiment.a2;
+
+import java.util.HashSet;
+
+public class b4 {
+    public static void main(String[] args){
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for(int i=2;i<=1000;i++){
+            hs.add(i);
+        }
+    }
+}
