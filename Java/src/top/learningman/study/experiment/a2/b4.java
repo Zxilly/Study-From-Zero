db3aa17581965e4d57aeede9d698f2e16cd7a7aa
zxilly <zhouxinyu1001@gmail.com>
2020-10-12 20:05:48 +0800

    format

--- Java/src/top/learningman/study/experiment/a2/b4.java
+++ Java/src/top/learningman/study/experiment/a2/b4.java
@@ -7,26 +7,26 @@ public class b4 {
     static ArrayList<Integer> prime = new ArrayList<>();
 
 
-    public static void main(String[] args){
-        for(int i=0;i<=1000;i++){
-            check[i]=true;
+    public static void main(String[] args) {
+        for (int i = 0; i <= 1000; i++) {
+            check[i] = true;
         }
 
-        for(int i=2;i<=1000;i++){
-            if(check[i]){
+        for (int i = 2; i <= 1000; i++) {
+            if (check[i]) {
                 prime.add(i);
             }
 
-            for(int j=0;j<prime.size()&&i*prime.get(j)<=1000;j++){
-                check[i*prime.get(j)]=false;
-                if(i%prime.get(j)==0){
+            for (int j = 0; j < prime.size() && i * prime.get(j) <= 1000; j++) {
+                check[i * prime.get(j)] = false;
+                if (i % prime.get(j) == 0) {
                     break;
                 }
             }
         }
 
-        for(int out:prime){
-            System.out.print(out+" ");
+        for (int out : prime) {
+            System.out.print(out + " ");
         }
         System.out.print("\n");
         System.out.println(prime.size());
