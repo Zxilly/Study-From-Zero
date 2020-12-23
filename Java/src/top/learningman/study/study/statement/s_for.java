1cd488eab278c15b1138aaaf67bf578588babb89
zxilly <zhouxinyu1001@gmail.com>
2020-12-23 11:02:33 +0800

    matrix

---
+++ Java/src/top/learningman/study/study/statement/s_for.java
@@ -0,0 +1,27 @@
+package top.learningman.study.study.statement;
+
+public class s_for {
+    public static void main(String[] args){
+        for (int i=1;i<5;i++){
+            for (int j=1;j<5;j++){
+                System.out.println("A");
+                if(j==2){
+                    break;
+                }
+            }
+            System.out.println("B");
+        }
+
+        System.out.println("-------------------------");
+
+        outer:for (int i=1;i<5;i++){
+            for (int j=1;j<5;j++){
+                System.out.println("A");
+                if(j==2){
+                    break outer;
+                }
+            }
+            System.out.println("B");
+        }
+    }
+}
