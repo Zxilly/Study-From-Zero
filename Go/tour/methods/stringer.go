e3d8e9f408cab229856260fbb5ca4a720c8f62cc
Zxilly <zhouxinyu1001@gmail.com>
2021-02-22 23:05:44 +0800

    go

--- Go/tour/methods/stringer.go
+++ Go/tour/methods/stringer.go
@@ -13,7 +13,7 @@ func (p Person) String() string { // 这玩意儿就是 python 的 __str__
 
 func main() {
 	a := Person{"Arthur Dent", 42}
-	z := Person{"Zaphod Beeblebrox", 9001}
+	z := Person{"Zahid Babbler", 9001}
 	fmt.Print(a)
 	fmt.Print(z)
 }
